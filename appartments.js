4cb73451c253075de9a7b0f50c8f61c21b4876f0
LucyJunior <53796420+LucyJunior@users.noreply.github.com>
2020-05-20 18:13:44 +0200

    update2

--- appartments.js
+++ appartments.js
@@ -2,10 +2,13 @@ var AppartmentsPage = `
 
 <section> 
   
-  <h1 class="tittle">Appartments</h1> 
-  <div> 
-  <img class="process" src="../img/brownlightbed.jpg" atl="thisishow">
-  <p> 1. </p>
+<h1 class="tittle">Appartments</h1> 
+<div> 
+<img class="process" src="../img/brownlightbed.jpg" atl="thisishow">
+<p> 1.Wide range of facilities</p>
+<p>2.Own bathroom and kitchen</p>
+<p>3.Only for Students</p>
+<p>4.No waiting list, free registration</p>
 
 </div>
 
--- howdoesitwork.js
+++ howdoesitwork.js
@@ -2,11 +2,45 @@
 
 var HowDoesItWorkPage =`
 <section> 
-  
   <h1 class="tittle">How Does it Work</h1> 
+
   <div> 
-  <img class="process" src="../img/brownlightspace.jpg" atl="thisishow">
-  <p> 1. You choose your future home</p>
+  <h3> Have you been assigned a studio via the automated draw? Then there will be an introductory meeting and eventually the digital signing of your contract.
+
+    In this step-by-step plan you can read exactly what needs to be done before you can move into your new studio.</h3>
+    
+    <p>1 E-MAIL CONFIRMATION
+    After you have been chosen via the automated draw, a confirmation will be sent by e-mail.
+    
+    You can then schedule an appointment for the introductory meeting via your account. Are you abroad? Then you can schedule a Skype meeting.</p>
+    
+    <p>2
+    MEETING CONVERSATION
+    An introductory meeting is held with the site manager on site or via Skype and consists of the following:
+    
+    Check for proof of enrollment at the educational institution;
+    Time to look around and visit the model home;
+    Time to ask questions.
+    The introductory meeting takes about 30 minutes. After the interview, you can accept or decline the studio online.</p>
+    
+    <p>3
+    DIGITAL SIGNING CONTRACT
+    You can digitally sign your contract via the Service Portal. Before you can receive the keys to your new studio, you have to pay the first rental bill.
+    
+    The first rental invoice consists of:
+    
+    The rent for the remaining days of the month in which the lease commences. Does the contract start at the end of the month? Then the rent for the next month is also included in the rental bill;
+    Administration costs of 15 euros;
+    The deposit of 1120 euros. </p>
+    <p>4
+    CHECK IN
+    During check-in you will be received at the reception by a location manager. The site manager walks you through the studio for verification and hands over the keys.
+    
+    Do you have any questions about the studio? Then the site manager will be happy to answer these.
+    
+    Please bring the following documents to check-in:
+    
+    Your ID (passport / ID card / driver's license)</p>
 
 </div>
 
